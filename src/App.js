3059a3232f8b4d082eabfbfccbbc7ba08a0d610c
Jaime Zegada <jzdesweb@gmail.com>
2021-11-17 03:48:21 -0400

    Shop2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
+import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container container-fluid">
+      <Route path="/" component={Home} exact />
+      <Route path="/product/:id" component={ProductDetails} exact />
+      </div>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
